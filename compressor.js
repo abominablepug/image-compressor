eb14e44f1bfcec7303b0071eb80e87e98db0bcab
abominablepug <esnahoum@gmail.com>
2025-09-10 12:23:49 -0400

    functionality complete

--- compressor.js
+++ compressor.js
@@ -1,3 +1,47 @@
 // Compress all images in a folder
 // Use sharp library
 // Input: path/to/folder
+const readline = require("readline");
+const sharp = require("sharp");
+const { parseArgs } = require("node:util");
+const { readdir, access, mkdir } = require("node:fs/promises");
+
+const options = {
+    height: {
+        type: "string",
+        short: "h"
+    },
+    input: {
+        type: "string",
+        short: "i"
+    },
+    width: {
+        type: "string",
+        short: "w"
+    },
+    output: {
+        type: "string",
+        short: "o"
+    }
+}
+
+const { height, width, input, output } = parseArgs({ options }).values
+
+async function main() {
+    const inputPath = process.cwd() + "/" + input;
+    const outputPath = process.cwd() + "/" + output;
+    try {
+        await access(outputPath);
+    } catch (err) {
+        console.log("Output folder does not exist... creating now");
+        await mkdir(outputPath);
+    }
+    const files = await readdir(inputPath);
+    files.map(file => {
+        sharp(inputPath + "/" + file)
+        .resize(Number(width), Number(height), {fit:"contain"})
+        .toFile(outputPath + "/" + file)
+    })
+}
+
+main();
