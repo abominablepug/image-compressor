ec3715ed594ff2dfaa1f4d812847d06b5009b368
abominablepug <esnahoum@gmail.com>
2025-09-12 17:32:24 -0400

    working on error handling

--- compressor.js
+++ compressor.js
@@ -27,21 +27,48 @@ const options = {
 
 const { height, width, input, output } = parseArgs({ options }).values
 
-async function main() {
-    const inputPath = process.cwd() + "/" + input;
-    const outputPath = process.cwd() + "/" + output;
-    try {
-        await access(outputPath);
-    } catch (err) {
-        console.log("Output folder does not exist... creating now");
-        await mkdir(outputPath);
+async function resizeImages(inputPath, outputPath, width, height) {
+    if (!width) {
+
     }
+
     const files = await readdir(inputPath);
+    const realHeight = height ? height : width;
     files.map(file => {
         sharp(inputPath + "/" + file)
         .resize(Number(width), Number(height), {fit:"contain"})
         .toFile(outputPath + "/" + file)
     })
+}
+
+async function main() {
+    const inputPath = process.cwd() + "/" + input;
+    const outputPath = (output) ? process.cwd() + "/" + output : inputPath;
+    try {
+        await access(outputPath);
+    } catch (err) {
+        console.log("Output folder does not exist... creating now");
+        try {
+            await mkdir(outputPath);
+            await resizeImages(inputPath);
+        } catch (err) {
+            if (err.code === "ENOENT") {
+                console.log("Part of the provided path does not exist.");
+            } else if (err.code === "EPERM") {
+                console.log("You do not have write permission to that folder.");
+            } else if (err.code === "ENAMETOOLONG") {
+                console.log("The name of the new folder is too long.");
+            } else {
+                console.log("Unable to resize the images. Make sure the right folder was provided and that ALL files within are images.");
+                console.log(err)
+            }
+        }
+    }
+    try {
+        await resizeImages(inputPath)
+    } catch (err) {
+        console.log("Unable to resize the images. Make sure the right folder was provided and that ALL files within are images.");
+    }
 }
 
 main();
